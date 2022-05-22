7fd37cca99d4264d404c148d9cbe6ef9cf7b2e87
Wafflgeasm <85540855+Wafflgeasm@users.noreply.github.com>
2022-05-21 19:08:49 -0500

    Interactable Objects implemented

---
+++ GhostGame/Assets/Scripts/Interaction Manager/Interactable.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Charging
+{
+    public class Interactable : MonoBehaviour
+    {
+        public bool isInRange;
+        public KeyCode interactKey;
+        public GameObject uiIcon;
+        public UnityEvent interactAction;
+
+        void Update()
+        {
+            if (isInRange)
+            {
+                uiIcon.SetActive(true);
+                if (Input.GetKeyDown(interactKey))
+                {
+                    interactAction.Invoke();
+                }
+            }else
+            {
+                uiIcon.SetActive(false);
+            }
+        }
+
+        private void OnTriggerEnter(Collider collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                isInRange = true;
+                Debug.Log("Player is in range");
+            }
+        }
+
+        private void OnTriggerExit(Collider collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                isInRange = false;
+                Debug.Log("Player is not in range");
+            }
+        }
+    }
+}
