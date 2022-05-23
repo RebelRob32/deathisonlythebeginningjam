198bd968ff938949a6e822faf177043a02eefcbb
Wafflgeasm <85540855+Wafflgeasm@users.noreply.github.com>
2022-05-22 20:10:20 -0500

    Assorted Changes
    Ghost shader changes, timer dial, score ui/script, and coatrack model.

---
+++ GhostGame/Assets/Scripts/Score TrackerTimer/timerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timerController : MonoBehaviour
+{
+    public GameObject dialSprite;
+    private float startingRotation;
+    private float currentRotation;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
