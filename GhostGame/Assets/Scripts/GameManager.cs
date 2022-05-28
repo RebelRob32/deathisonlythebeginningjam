ed05ee6948b82544e39b6b6c319aa4f38986b37e
RebelRob32 <r.gue89@protonmail.com>
2022-05-28 00:21:59 -0500

    Still Working on things

--- GhostGame/Assets/Scripts/GameManager.cs
+++ GhostGame/Assets/Scripts/GameManager.cs
@@ -7,11 +7,15 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public Text score;
+    public GameObject[] humans;
+    public GameObject winPanel;
+    public GameObject losePanel;
+    public float timer;
 
 
     public void Awake()
     {
-        
+        Time.timeScale = 1;
     }
 
     public void Start()
@@ -21,11 +25,29 @@ public class GameManager : MonoBehaviour
 
     public void Update()
     {
-        
+        timer -= Time.deltaTime;
+        humans = GameObject.FindGameObjectsWithTag("Human");
     }
 
     public void GetScore()
     {
 
+    }
+
+    public void WinCondition()
+    {
+        int numberOfHumans = humans.Length;
+        if(numberOfHumans == 0)
+        {
+            //win
+        }
+    }
+
+    public void LoseCondition()
+    {
+        if (timer <= 0)
+        {
+            Time.timeScale = 0;
+        }
     }
 }
